66eab0b983a85dffcff911fe36d2a49b5505a4ed
batu4404 <batu4404@gmail.com>
2017-11-24 19:15:29 +0700

    Vector icon

--- app/index.js
+++ app/index.js
@@ -1,13 +1,21 @@
 import React, {Component} from 'react';
 import * as flex from './learn/Flex';
-import Touchables from './learn/Touchables';
-import UserInterface from './learn/UserInterface';
-import LoginScreen from './screens/LoginScreen';
+import {StackNavigator} from 'react-navigation';
+import SimpleLogin from './screens/login/SimpleLogin';
+import YoutubeLike from './screens/google/YoutubeLike';
 
+const RootNavigator = StackNavigator({
+    Login: {screen: SimpleLogin},
+    YoutubeLike: {screen: YoutubeLike}
+}, {
+    initialRouteName: 'Login',
+    headerMode: 'none'
+})
+ 
 export default class App extends Component {
     render() {
         return (
-            <LoginScreen />
+            <RootNavigator />
         )
     } 
 }
